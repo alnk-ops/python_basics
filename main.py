d55f778c452d684bd0b0b30aa726c97ca1d87711
alnk-ops <alnk05711@gmail.com>
2026-01-02 12:02:36 +0530

    first commit

---
+++ main.py
@@ -0,0 +1,70 @@
+print("Hello, World!")
+print("welcome to the python programming language")
+a=10
+b=20
+c=a+b
+print("The sum of a and b is:", c)
+print(type(c))
+
+# input to the user
+# name=input("Enter your name: ")
+# print("Hello ", name , "welcome to Python programming!")
+# print(type(name))
+# convert int to string
+age="100"
+print(age)
+print(type(age))
+
+# logical operators
+x=7
+print(x)
+print(x<5 and x<10)
+print(x<5 or x<10)
+print(not(x<5 and x<10))
+# comparison operators
+print("comparison operators in python")
+print(x==7)
+print(x!=7)
+print(x>5)
+print(x<5)
+print(x>=7)
+print(x<=7)
+print("assignment operators in python")
+y=5
+y+=3
+print(y)
+y-=2
+print(y)
+y*=2
+print(y)
+y/=2
+print(y)
+y%=2
+print(y)
+y//=2
+print(y)
+y**=2
+print(y)
+
+print("bitwise operators in python")
+a=10
+b=4
+print(a & b)  # AND
+print(a | b)  # OR  
+print(a ^ b)  # XOR
+print(~a)     # NOT
+print(a << 2) # Left Shift
+print(a >> 2) # Right Shift
+
+# identity operators 
+print("identity operators in python")
+m=10
+a=20
+c=m
+z=
+print(m is c)
+print(m == c)
+print(m is a)
+print(m != a)
+print (m is not a)
+print(m != c)
